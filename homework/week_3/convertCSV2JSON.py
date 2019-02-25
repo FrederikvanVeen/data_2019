71cbd6abb84d9a0887611111b6b5625ff597ed02
Frederik van Veen <frederikvanveen@Frederiks-MacBook-Pro.local>
2019-02-25 15:43:48 +0100

    added changes to csv2json

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,27 +1,24 @@
-# import csv
-# import json
-# INPUT_CSV = "CausesOfDeath_France_2001-2008.csv"
-# csvfile = open(INPUT_CSV, 'r')
-# jsonfile = open('output.json', 'w')
-#
-# fieldnames = ("FirstName","LastName","IDNumber","Message")
-#
-# reader = csv.DictReader( csvfile, fieldnames)
-# for row in reader:
-#     json.dump(row, jsonfile)
-#     jsonfile.write('\n')
-
-import csv
+åimport csv
 import json
 
-# open the CSV
-csv_infile = open( 'CausesOfDeath_France_2001-2008.csv', 'rU' )
+INPUTCSV = 'CausesOfDeath_France_2001-2008.csv'
+OUTPUTJSON = 'CausesOfDeath_France_2001-2008.json'
 
-# read csv file into DictReader object
-reader = csv.DictReader(csv_infile)
 
-# Parse the CSV into JSON
-out = json.dumps( [ row for row in reader ] )
-# Save the JSON
-json_outfile = open( 'output.json', 'w')
-json_outfile.write(out)
+def csvtojson(INPUTCSV):
+    # open the CSV
+    csv_infile = open( 'CausesOfDeath_France_2001-2008.csv', 'rU' )
+
+    # read csv file into DictReader object
+    reader = csv.DictReader(csv_infile)
+
+    # Parse the CSV into JSON
+    out = json.dumps( [ row for row in reader ] )
+
+    # Save the JSON
+    json_outfile = open(OUTPUTJSON, 'w')
+    json_outfile.write(out)
+
+
+if __name__ == "__main__":
+    csvtojson(INPUTCSV)
